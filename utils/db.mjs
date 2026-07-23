4c44580503a46d2ef0c82d83faaed98b570a008f
Matzuc2 <9793@holbertonstudents.com>
2026-07-23 17:19:06 +0200

    feat: implement DBClient for MongoDB connection and data retrieval

---
+++ utils/db.mjs
@@ -0,0 +1,26 @@
+import mongodb from 'mongodb';
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+class DBClient {
+  constructor() {
+    mongodb.MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      this.db = client.db(database);
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.files.countDocuments();
+  }
+}
+export default new DBClient();
