3ef29cebad659a7e5b1a817c441fc25c2fa74ac2
Matzuc2 <9793@holbertonstudents.com>
2026-07-23 16:23:45 +0200

    fix: refactor RedisClient methods to use promisify for better error handling

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,4 +1,5 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
     constructor() {
@@ -13,20 +14,15 @@ class RedisClient {
     }
 
     async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, value) => {
-                if (err) reject(err);
-                else resolve(value);
-            });
-        });
+        return promisify(this.client.get).bind(this.client)(key);
     }
 
     async set(key, value, duration) {
-       return this.client.setex(key, duration, value);
+        return promisify(this.client.setex).bind(this.client)(key, duration, value);
     }
 
     async del(key) {
-        this.client.del(key);
+        return promisify(this.client.del).bind(this.client)(key);
     }
 }
 
