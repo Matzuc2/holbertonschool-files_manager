67c4bc49de79f1390f0685eb9dddfb9428d205d6
Matzuc2 <9793@holbertonstudents.com>
2026-07-23 16:29:12 +0200

    fix: format RedisClient methods for consistent indentation

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -2,28 +2,28 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient()
-            .on('error', (err) => {
-                console.log(err);
-            });
-    }
+  constructor() {
+    this.client = redis.createClient()
+      .on('error', (err) => {
+        console.log(err);
+      });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return promisify(this.client.get).bind(this.client)(key);
-    }
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
 
-    async set(key, value, duration) {
-        return promisify(this.client.setex).bind(this.client)(key, duration, value);
-    }
+  async set(key, value, duration) {
+    return promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
 
-    async del(key) {
-        return promisify(this.client.del).bind(this.client)(key);
-    }
+  async del(key) {
+    return promisify(this.client.del).bind(this.client)(key);
+  }
 }
 
 export default new RedisClient();
