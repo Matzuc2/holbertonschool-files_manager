7c9bd599d5610570ecdc8e27e79bc60642b6ebc3
Matzuc2 <9793@holbertonstudents.com>
2026-07-23 15:59:48 +0200

    fix: update redis client import and adjust connection handling

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import redisClient from './utils/redis';
 
 (async () => {
-    console.log(redisClient)
     console.log(redisClient.isAlive());
     console.log(await redisClient.get('myKey'));
     await redisClient.set('myKey', 12, 5);
--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,36 +1,32 @@
-import { createClient } from 'redis';
+import redis from 'redis';
 
-class RedisClient{
-    constructor(){
-        this.client = createClient()
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient()
             .on('error', (err) => {
-                console.log(err)
-            })
-        this.client.connect()
-    }
-    isAlive(){
-            if(this.client.isReady){
-
-                return true
-            }
-            return false
-        
-    }
-    async get(key){
-        try{
-            const value = this.client.get(key)
-            return value
-        }
-        catch{
-            return null
-        }
-    }
-    async set(key, value, duration){
-        return this.client.set(key,value, {EX: duration})
+                console.log(err);
+            });
     }
 
-    async del(key){
-       return this.client.del(key)
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, value) => {
+                if (err) reject(err);
+                else resolve(value);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+       return this.client.setex(key, duration, value);
+    }
+
+    async del(key) {
+        this.client.del(key);
     }
 }
 
