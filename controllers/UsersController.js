31fa618c70f622b2c8f626c1029ad97e283635c2
Matzuc2 <9793@holbertonstudents.com>
2026-07-24 13:28:52 +0200

    feat: implement user registration endpoint with password hashing

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+import cryptoSample from 'crypto';
+import db from '../utils/db';
+
+function hashPassword(password) {
+  const hashValue = cryptoSample.createHash('sha1');
+  hashValue.update(password);
+  return hashValue.digest('hex');
+}
+
+exports.postNew = async (req, res) => {
+  const { email } = req.body; // destructurarion ES6
+  let { password } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+  const existingUser = await db.db.collection('users').findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      error: 'Already exist',
+    });
+  }
+  password = hashPassword(password);
+  const result = await db.db.collection('users').insertOne({
+    email,
+    password,
+  });
+  const { insertedId } = result;
+  return res.status(201).json({
+    insertedId,
+    email,
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
@@ -8,4 +9,5 @@ router.get('/status', AppController.getStatus);
 router.get('/stats',
   AppController.getStats);
 
+router.post('/users', UsersController.postNew);
 module.exports = router;
